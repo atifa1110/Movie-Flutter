0c9c09addfd5d8b671877017e7795fd7ac942a77
atifa1110 <atifafiorenza24@gmail.com>
2024-12-09 14:41:03 +0700

    add integration coverage

--- movies/lib/presentation/pages/home_movie_page.dart
+++ movies/lib/presentation/pages/home_movie_page.dart
@@ -3,6 +3,7 @@ import 'package:core/domain/entities/movie.dart';
 import 'package:core/presentation/about_page.dart';
 import 'package:core/presentation/watchlist_page.dart';
 import 'package:core/utils/constants.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies/presentation/pages/popular_movies_page.dart';
